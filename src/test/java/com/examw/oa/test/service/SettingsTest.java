60f0697a2724d07552e97862a1df6acf09e0ce7a
jeasonyoung <jeason1914@qq.com>
2014-07-01 09:45:45 +0800

    添加测试类

--- src/test/java/com/examw/oa/test/service/SettingsTest.java
+++ src/test/java/com/examw/oa/test/service/SettingsTest.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.examw.oa.domain.plan.Settings;
 import com.examw.oa.service.plan.ISettingsService;
@@ -14,11 +16,12 @@ import com.thoughtworks.xstream.XStream;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-examw-oa.xml"})
+@Transactional
 public class SettingsTest {
 	//settings
 	@Resource
 	private ISettingsService settingsService;
-	//@Test
+	@Test
 	public void testBitAndFunction(){
 		List<Settings> list =  this.settingsService.findSettings(Settings.TYPE_DAY);
 		XStream xStream = new XStream();
